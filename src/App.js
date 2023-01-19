13272c15f7e42caa57bb6fac044d27c9320cc993
jiyean99 <wldus019113@icloud.com>
2023-01-19 23:11:27 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from "react";
+// import Items from "./components/Items";
+// import Header from "./components/Header";
+// import CreateItem from "./components/CreateItem";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//         <div className="App">
+//             <Header/>
+//             <Routes>
+//                 <Route path="/" element={<Items/>}/>
+//                 <Route path="/todo" element={<Items/>}/>
+//                 <Route path="/done" element={<Items/>}/>
+//                 <Route path="/create_item" element={<CreateItem/>}/>
+//             </Routes>
+//         </div>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App;
+
+import React from "react";
+import Items from "./component/Items";
+import Header from "./component/Header";
+import CreateItem from "./component/CreateItem";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Items status="todo&status=done"/>} />
+          <Route path="/todo" element={<Items status="todo" />} />
+          <Route path="/done" element={<Items status="done" />} />
+          <Route path="/create_item" element={<CreateItem />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
-
-export default App;
---
+++ src/components/Items.js
@@ -0,0 +1,27 @@
+export default function Items() {
+  return (
+    <table>
+      <td>Task</td>
+      <td>Due</td>
+      <td></td>
+      <tr>
+        <td></td>
+        <td>커피마시기</td>
+        <td>20230331</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>커피마시기</td>
+        <td>20230331</td>
+        <td></td>
+      </tr>
+      <tr>
+        <td></td>
+        <td>커피마시기</td>
+        <td>20230331</td>
+        <td></td>
+      </tr>
+    </table>
+  );
+}
